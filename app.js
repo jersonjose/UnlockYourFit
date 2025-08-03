fb6375fd25be53d4418b4b665701ac0ed7f37284
jersonjose <jersonmenguito@gmail.com>
2025-08-03 22:25:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -72,6 +72,16 @@ function calculateBmi() {
 };
 
 
+function clearBmi() {
+  age.value = "";
+  height.value = "";
+  weight.value = "";
+  document.getElementById("male").checked = false;
+  document.getElementById("female").checked = false;
+  document.querySelector("#result").innerHTML = "00.00 kg/m<sup>2</sup>";
+  document.querySelector(".comment").innerHTML = "";
+};
+
 var ageBmr = document.getElementById("age-bmr");
 var heightBmr = document.getElementById("height-bmr");
 var weightBmr = document.getElementById("weight-bmr");
@@ -129,14 +139,41 @@ function calculateBmr() {
 
 };
 
-clearButtonBmr.addEventListener("click", function () {
+function clearBmr() {
   ageBmr.value = "";
   heightBmr.value = "";
   weightBmr.value = "";
   document.getElementById("male-bmr").checked = false;
   document.getElementById("female-bmr").checked = false;
-  document.querySelector("#bmr-result").innerHTML = "00.00 kg/m<sup>2</sup>";
-  document.querySelector(".comment-bmr").innerHTML = "";
-});
+  document.querySelector("#bmr-result").innerHTML = "0 Calories/day";
+  // document.querySelector(".comment-bmr").innerHTML = "";
+};
 
 
+// Page Switcher JS
+
+window.onload = () => {
+    const tab_switchers = document.querySelectorAll('[data-switcher]');
+
+    for (let i = 0; i < tab_switchers.length; i++) {
+        const tab_switcher = tab_switchers[i];
+        const page_id = tab_switcher.dataset.tab;
+
+        tab_switcher.addEventListener('click', () => {
+            document.querySelector('.tabs .tab.is-active').classList.remove('is-active');
+            tab_switcher.parentNode.classList.add('is-active');
+
+            SwitchPage(page_id);
+        });
+    }
+}
+
+function SwitchPage (page_id) {
+    console.log(page_id);
+
+    const current_page = document.querySelector('.pages .page.is-active');
+    current_page.classList.remove('is-active');
+
+    const next_page = document.querySelector(`.pages .page[data-page="${page_id}"]`);
+    next_page.classList.add('is-active');
+}
